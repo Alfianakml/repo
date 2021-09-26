f43cfc7ff7a47e853663aaa09934fdc00716c7dd
alfianakml <alfianakmal@student.telkomuniversity.ac.id>
2021-09-26 17:54:07 +0700

    HelloWorld.java

---
+++ prak1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main (String[] args) {
+		System.out.println("Nama  : Alfian!");
+		System.out.println("NIM   : 6706210147");
+		System.out.println("Kelas : RPLA 45-01");
+	}
+}
