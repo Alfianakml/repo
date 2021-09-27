0b0573ffc48e890864ca07c3c2e78cbbe2e1b680
Alfianakml <91175540+Alfianakml@users.noreply.github.com>
2021-09-27 16:52:48 +0700

    Delete prak1 directory

--- prak1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-	public static void main (String[] args) {
-		System.out.println("Nama  : Alfian!");
-		System.out.println("NIM   : 6706210147");
-		System.out.println("Kelas : RPLA 45-01");
-	}
-}
